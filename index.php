1e505a299a6c8692d20e06f76f13ec844452336b
dejvik47 <pasekdavid8@gmail.com>
2023-12-13 15:14:51 +0100

    GitHubTest
    Test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="sign_in.css">
+    <title>Test file</title>
+</head>
+    <body>
+        <?php include "header.html";?>
+    </body>
+</html>
---
+++ sign_in.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="header.css">
+    <link rel="stylesheet" href="sign_in.css">
+    <title>Test file</title>
+</head>
+    <body>
+        <?php include "header.html";?>
+
+        <div class="container">
+            <form class=""action="POST">
+                <span>Sign in</span>
+            </form>
+        </div>
+    </body>
+</html>
