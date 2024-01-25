f62dccbd6181c7c87864ae444c0b95a22b96007b
dejvik47 <pasekdavid8@gmail.com>
2024-01-25 05:29:23 +0100

    Preparation of main page
    main 4 sections added
    preview pictures added
    basic layout
    without functionality

--- index.php
+++ index.php
@@ -5,9 +5,20 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="header.css">
     <link rel="stylesheet" href="sign_in.css">
+    <link rel="stylesheet" href="main.css">
     <title>Test file</title>
 </head>
     <body>
         <?php include "header.html";?>
+
+        <div class="options">
+
+        <div class="option1"><img src="checklist.png" alt="todolist-img" class="todolistImg"></div>
+        <div class="option2"><img src="user.png" alt="profile-pic-img" class="profileImg"></div>
+        <div class="option3"><img src="photo-album.png" alt="gallery-pic-img" class="galleryImg"></div>
+        <div class="option4"><img src="notes.png" alt="posts" class="postsImg"></div>
+
+        </div>
+
     </body>
 </html>
